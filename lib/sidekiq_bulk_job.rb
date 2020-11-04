fa52c6af7d7c791cf66cbd6d4107ddda8a969b90
benko <benko@benkode-Mac-mini.local>
2020-11-04 14:33:41 +0800

    first commit

---
+++ lib/sidekiq_bulk_job.rb
@@ -0,0 +1,6 @@
+require "sidekiq_bulk_job/version"
+
+module SidekiqBulkJob
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sidekiq_bulk_job/version.rb
@@ -0,0 +1,3 @@
+module SidekiqBulkJob
+  VERSION = "0.1.0"
+end
