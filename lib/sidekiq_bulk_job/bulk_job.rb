c445af3f0247a190d4bc1ab50f881f21a7962386
scalaview <chailink100@gmail.com>
2021-08-16 17:15:23 +0800

    retry in batch job

--- lib/sidekiq_bulk_job/bulk_job.rb
+++ lib/sidekiq_bulk_job/bulk_job.rb
@@ -11,6 +11,7 @@ module SidekiqBulkJob
     def perform(job_class_name, args_array)
       target_name, method_name = SidekiqBulkJob::Utils.split_class_name_with_method job_class_name
       job = SidekiqBulkJob::Utils.constantize(target_name)
+      fail_args = []
       args_array.each do |_args|
         begin
           args = SidekiqBulkJob::Utils.load _args
@@ -20,10 +21,14 @@ module SidekiqBulkJob
             job.new.send(method_name, *args)
           end
         rescue Exception => e
+          fail_args << _args
           SidekiqBulkJob.logger.error("#{job_class_name} Args: #{args}, Error: #{e.full_message}")
           SidekiqBulkJob.fail_callback(job_class_name: job_class_name, args: args, exception: e)
-          SidekiqBulkJob::JobRetry.new(job, args, e).push
+          # SidekiqBulkJob::JobRetry.new(job, args, e).push
         end
+      end
+      if fail_args.present?
+        SidekiqBulkJob::JobRetry.new(job_class_name, fail_args, e).push
       end
     end
   end
